93bcc993ca7cc16b4ec6a85ede1fe42d2ae2ddf6
jgilme01 <jgilme01@seattlecentral.edu>
2017-03-04 14:22:46 -0800

    Stored SQL query results into objects for better use

--- news/news_view.php
+++ news/news_view.php
@@ -1,19 +1,9 @@
 <?php
 /**
- * survey_view.php is a page to demonstrate the proof of concept of the 
- * initial SurveySez objects.
- *
- * Objects in this version are the Survey, Question & Answer objects
- * 
- * @package SurveySez
- * @author William Newman
- * @version 2.12 2015/06/04
- * @link http://newmanix.com/ 
- * @license http://www.apache.org/licenses/LICENSE-2.0
- * @see Question.php
- * @see Answer.php
- * @see Response.php
- * @see Choice.php
+ * @package News
+ * @author J Gilmer
+ * @3.4.17
+ * @see index.php in "news" directory
  */
  
 require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
@@ -46,44 +36,57 @@ $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::
 if(mysqli_num_rows($result) > 0){
     $config->titleTag = "'" . $result->fetch_field_direct(2)->name . "' Feed!";
 
-
-/*$mySurvey = new SurveySez\Survey($myID); //MY_Survey extends survey class so methods can be added
-if($mySurvey->isValid)
-{
-	$config->titleTag = "'" . $mySurvey->Title . "' Feed!";
-}else{
-	$config->titleTag = smartTitle(); //use constant 
-}*/
 #END CONFIG AREA ---------------------------------------------------------- 
-
 get_header(); #defaults to theme header or header_inc.php
     
-    
-//$obj = mysqli_fetch_object($result);
-//$fieldinfo=mysqli_fetch_field_direct($result,0);
-//mysqli_store_result (mysqli_query(IDB::conn(),$sql));
-//echo '<h3>' . $obj->CategoryName . '</h3>';
-     
 
-     echo '<table class="table table-striped">';
+
+foreach($result as $row){#objects of class row from SQL rows
+    //$row = new stdClass;
+    $CategoryName = $row['CategoryName'];
+    $FeedID = $row['FeedID'];
+    $FeedName = $row['FeedName'];
+    $currentRow = new Row($FeedID, $FeedName, $CategoryName);
+    $config->rows[]=$currentRow;
+
+}
+
+ echo '<table class="table table-striped">';
     echo '<tr>';
-        echo '<th>' . $result->fetch_field_direct(2)->name . '</th>';
-        echo '</tr>';
-    //    mysqli_use_result(mysqli_query(IDB::conn(),$sql));
-         while($row = mysqli_fetch_assoc($result))
-	{# pull data from associative array
+        echo '<th><a href="' . VIRTUAL_PATH . 'news/index.php' . '">Categories</a>: ' . $CategoryName . '</th></tr>';
+    #two for each loops to separate $CategoryName
     
+
+     
+foreach($config->rows as $row)
+{#loop through each object
     echo '<tr>';
-        echo '<td>' . $row['CategoryName'] . ' -> <a href="placeholder.php?id=' . $row['FeedID'] . '">' . $row['FeedName'] . '</a></td></tr>';
+        echo '<td><a href="placeholder.php?id=' . $row->FeedID . '">' . $row->FeedName . '</a></td></tr>';
         }
     echo '</table>';
 
 
 
+
 get_footer(); #defaults to theme footer or footer_inc.php
-}
+}#end if block $result>0
 else{//no subcategory!
     header('Location: ' . VIRTUAL_PATH . 'news/index.php');
+}
+
+class Row
+{
+    public $FeedID = 0;
+    public $FeedName = '';
+    public $CategoryName = '';
+
+        public function __construct($FeedID, $FeedName, $CategoryName){
+            $this->FeedID = $FeedID;
+            $this->FeedName = $FeedName;
+            $this->CategoryName = $CategoryName;
+            
+        }
+
 }
 
 
